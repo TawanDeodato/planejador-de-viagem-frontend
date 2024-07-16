ac2863edfd5c0805c6c6186f26539bb31ada2372
TawanDeodato <tawandeodato94@gmail.com>
2024-07-16 13:17:11 -0300

    Adicionando algumas conexões com a api

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,34 +1,54 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
+interface Participant {
+  id: string;
+  name: string | null;
+  email: string;
+  is_confirmed: boolean;
+}
 
 export function Guests() {
-    return (
-        <div className="space-y-6">
-            <h2 className="font-semibold text-xl">Convidados</h2>
-            <div className="space-y-5">
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium texxt-zinc-100">Jessica White</span>
-                        <span  className="text-sm text-zinc-400 truncate">
-                            jessica.white44@yahoo.com
-                        </span>
-                    </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
-                </div>
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium texxt-zinc-100">Dr. Rita Pacocha</span>
-                        <span className="text-sm text-zinc-400 truncate">
-                            lacy.stiedemann@gmail.com
-                        </span>
-                    </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0"/>
-                </div>
+  const { tripId } = useParams();
+  const [participants, setParticiapants] = useState<Participant[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/participants`)
+      .then((response) => setParticiapants(response.data.participants));
+  }, [tripId]);
+
+  return (
+    <div className="space-y-6">
+      <h2 className="font-semibold text-xl">Convidados</h2>
+      <div className="space-y-5">
+        {participants.map((participant, index) => {
+          return (
+            <div key={participant.id} className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium texxt-zinc-100">
+                  {participant.name ?? `Convidado ${index}`}
+                </span>
+                <span className="text-sm text-zinc-400 truncate">
+                  {participant.email}
+                </span>
+              </div>
+              {participant.is_confirmed ? (
+                <CheckCircle2 className="size-5 shrink-0 text-green-400" />
+              ) : (
+                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              )}
             </div>
-            <Button variant="secondary" size="full">
-                <UserCog className='size-5'/>
-                Gerenciar convidados
-            </Button>
-        </div>
-    )
+          );
+        })}
+      </div>
+      <Button variant="secondary" size="full">
+        <UserCog className="size-5" />
+        Gerenciar convidados
+      </Button>
+    </div>
+  );
 }
